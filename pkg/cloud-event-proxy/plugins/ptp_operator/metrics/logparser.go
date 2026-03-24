redhat-cne/cloud-event-proxy

OCPBUGS-62812: Propogate FREERUN for CLOCK_REALTIME

plugins/ptp_operator/metrics/logparser.go
@@ -661,6 +661,7 @@ func (p *PTPEventManager) ParseGNSSLogs(processName, configName, output string, 
 			log.Infof("%s last state %s and current state %s", processName, lastState, GetSyncState(syncState))
 			masterResource := fmt.Sprintf("%s/%s", alias, MasterClockType)
 			p.publishGNSSEvent(gnssState, gnssOffset, GetSyncState(syncState), masterResource, ptp.GnssStateChange)
+			p.lastOverallGnssState = GetSyncState(syncState)
 		}
 	}
 }

plugins/ptp_operator/metrics/manager.go
@@ -40,6 +40,7 @@ type PTPEventManager struct {
 	// Ptp4lConfigInterfaces holds interfaces and its roles, after reading from ptp4l config files
 	Ptp4lConfigInterfaces map[types.ConfigName]*ptp4lconf.PTP4lConfig
 	lastOverallSyncState  ptp.SyncState
+	lastOverallGnssState  ptp.SyncState
 	processStates         map[types.ConfigName]map[string]bool
 }
 

plugins/ptp_operator/metrics/metrics.go
@@ -228,6 +228,10 @@ func (p *PTPEventManager) ExtractMetrics(msg string) {
 			if processName != ts2phcProcessName && !(interfaceName == master || interfaceName == ClockRealTime) {
 				return // only master and clock_realtime are supported
 			}
+			// Force FREERUN if gnss is FREERUN and ts2phc is the source
+			if processName != ts2phcProcessName && masterOffsetSource == ts2phcProcessName && p.lastOverallGnssState == ptp.FREERUN {
+				syncState = ptp.FREERUN
+			}
 			offsetSource := master
 			if strings.Contains(output, "sys offset") {
 				offsetSource = sys

redhat-cne/upstream-ptp-operator-monorepo

pkg/cloud-event-proxy/plugins/ptp_operator/metrics/logparser.go
@@ -412,6 +412,7 @@ func (p *PTPEventManager) ParseGMLogs(processName, configName, output string, fi
 		log.Infof("%s sync state %s, last ptp state is : %s", masterResource, clockState.State, lastClockState)
 		ptpStats[masterType].SetLastSyncState(clockState.State)
 		p.PublishEvent(clockState.State, lastOffset, masterResource, ptp.PtpStateChange)
+		p.lastOverallGMState = GetSyncState(syncState)
 		UpdateSyncStateMetrics(processName, aliasValue, ptpStats[masterType].LastSyncState())
 		UpdatePTPOffsetMetrics(processName, processName, aliasValue, float64(lastOffset))
 	}

pkg/cloud-event-proxy/plugins/ptp_operator/metrics/manager.go
@@ -40,6 +40,7 @@ type PTPEventManager struct {
 	// Ptp4lConfigInterfaces holds interfaces and its roles, after reading from ptp4l config files
 	Ptp4lConfigInterfaces map[types.ConfigName]*ptp4lconf.PTP4lConfig
 	lastOverallSyncState  ptp.SyncState
+	lastOverallGMState    ptp.SyncState
 	processStates         map[types.ConfigName]map[string]bool
 }
 

pkg/cloud-event-proxy/plugins/ptp_operator/metrics/metrics.go
@@ -227,6 +227,10 @@ func (p *PTPEventManager) ExtractMetrics(msg string) {
 			if processName != ts2phcProcessName && !(interfaceName == master || interfaceName == ClockRealTime) {
 				return // only master and clock_realtime are supported
 			}
+			// Force FREERUN if gnss is FREERUN and ts2phc is the source
+			if processName != ts2phcProcessName && masterOffsetSource == ts2phcProcessName && p.lastOverallGMState == ptp.FREERUN {
+				syncState = ptp.FREERUN
+			}
 			offsetSource := master
 			if strings.Contains(output, "sys offset") {
 				offsetSource = sys